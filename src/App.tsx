0053f99a73ac34805144ac0db135bddb5df8265d
DYMOND123 <158254576+DYMOND123@users.noreply.github.com>
2024-02-22 12:19:44 +0200

    Laborator2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyCard from './MyCard';
+import PersonalDataForm from './PersonalDataForm';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Personal Data Management</h1>
+      <PersonalDataForm />
+      <MyCard />
     </div>
   );
-}
+};
 
 export default App;
